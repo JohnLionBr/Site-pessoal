f53f4966823b075fb213cbbafcc44b17673838ac
JohnLionBr <jpsouzaleao@gmail.com>
2021-09-13 09:13:39 -0300

    Aprimorando

---
+++ learning.js
@@ -0,0 +1,23 @@
+function html() {
+    window.alert('HTML abreviação para a expressão inglesa HyperText Markup Language, que significa: "Linguagem de Marcação de Hipertexto" é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores.')
+}
+
+function css() {
+    window.alert('Cascading Style Sheets é um mecanismo para adicionar estilo a um documento web. O código CSS pode ser aplicado diretamente nas tags ou ficar contido dentro das tags <style>. Também é possível, em vez de colocar a formatação dentro do documento, criar um link para um arquivo CSS que contém os estilos.')
+}
+
+function js() {
+    window.alert('JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.')
+}
+
+function git(){
+    window.alert('Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo.')
+}
+
+function github() {
+    window.alert('GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. Ele permite que programadores, utilitários ou qualquer usuário cadastrado na plataforma contribuam em projetos privados e/ou Open Source de qualquer lugar do mundo.')
+}
+
+function android() {
+    window.alert
+}
